b144cba8a49db041b2c77973dd7b3454d39ef663
Kim Seongwon <91422659+seongwon02@users.noreply.github.com>
2026-07-21 23:51:40 +0900

    [level 3] Title: 표 편집, Time: 218.64 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.py
+++ 프로그래머스/3/81303. 표 편집/표 편집.py
@@ -1,47 +1,51 @@
 def solution(n, k, cmd):
     answer = ["O"] * n
-    updown = [[i-1, i+1] for i in range(n)]
     stack = []
+    up = [i-1 for i in range(n)]
+    down = [i+1 for i in range(n)]
     
-    for p in cmd:
-        if p[0] == 'U':
-            _, x = p.split(' ')
-            for _ in range(int(x)):
-                k = updown[k][0]
-        elif p[0] == 'D':
-            _, x = p.split(' ')
+    for i in range(len(cmd)):
+        if cmd[i][0] == 'D':
+            _, x = cmd[i].split()
+            x = int(x)
             
-            for _ in range(int(x)):
-                k = updown[k][1]
-        elif p[0] == 'C':
-            stack.append(k)
+            for _ in range(x):
+                k = down[k]
+        elif cmd[i][0] == 'U':
+            _, x = cmd[i].split()
+            x = int(x)
+            
+            for _ in range(x):
+                k = up[k]
+        elif cmd[i][0] == 'C':
+            u = up[k]
+            d = down[k]
+            
+            stack.append((k, u, d))
             answer[k] = 'X'
-            
-            prev = updown[k][0]
-            nxt = updown[k][1]
-            
-            if prev != -1:
-                updown[prev][1] = nxt
-            if nxt != n:
-                updown[nxt][0] = prev
-                
-            if nxt == n:
-                k = prev
+                         
+            if d == n:
+                k = u
+                down[u] = d
+            elif u == -1:
+                k = d
+                up[d] = u
             else:
-                k = nxt
-                
-        elif p[0] == 'Z':
-            r = stack.pop()
-            answer[r] = 'O'
+                k = d
+                down[u] = d
+                up[d] = u
+        elif cmd[i][0] == 'Z':
+            idx, u, d = stack.pop()
+            answer[idx] = 'O'
             
-            prev = updown[r][0]
-            nxt = updown[r][1]
-            
-            if prev != -1:
-                updown[prev][1] = r
-            
-            if nxt != n:
-                updown[nxt][0] = r
+            if u == -1:
+                up[d] = idx
+            elif d == n:
+                down[u] = idx
+            else:
+                up[d] = idx
+                down[u] = idx
+                         
             
     return "".join(answer)
         
